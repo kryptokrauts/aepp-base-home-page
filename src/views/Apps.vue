dd4a5418edbf60ba1b74c10b1b3d31f00632a4c3
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-12-12 09:48:57 +0100

    Integrate with Base app

--- src/views/Apps.vue
+++ src/views/Apps.vue
@@ -10,9 +10,10 @@
       <ListItem
         v-for="(app, idx) in aeternityApps"
         :key="`list-item-aeternity-app-${idx}`"
-        :to="app.url"
         :title="app.name"
         :subtitle="app.path"
+        :to="getLink(app.path)"
+        @click="getClickHandler(app.path)"
       >
         <img
           v-if="app.icon"
@@ -31,7 +32,8 @@
           v-for="(app, idx) in bookmarkedApps"
           :key="`app-shortcut-aeternity-app-${idx}`"
           v-bind="app"
-          :to="app.url"
+          :to="getLink(app.path)"
+          @click="getClickHandler(app.path)"
         />
       </div>
     </template>
@@ -40,6 +42,7 @@
 
 <script>
 import { mapState } from 'vuex';
+import RpcClient from '@aeternity/aepp-sdk/es/rpc/client';
 import { PROTOCOL_DEFAULT } from 'aepp-base/src/lib/constants';
 import { aeternityAppsPaths } from 'aepp-base/src/lib/appsRegistry';
 import MobilePage from 'aepp-base/src/components/mobile/Page.vue';
@@ -48,11 +51,10 @@ import AppShortcut from 'aepp-base/src/components/AppShortcut.vue';
 import AeCard from 'aepp-base/src/components/AeCard.vue';
 import ListItem from 'aepp-base/src/components/ListItem.vue';
 
-const pathsToMetadata = paths => (state, getters) => paths
-  .map(path => ({
-    ...getters['appsMetadata/get'](path),
-    url: `${PROTOCOL_DEFAULT}//${path}`,
-  }));
+const pathsToMetadata = (paths, getters) => paths.map(path => ({
+  ...getters['appsMetadata/get'](path),
+  path,
+}));
 
 export default {
   components: {
@@ -62,11 +64,32 @@ export default {
     AeCard,
     ListItem,
   },
-  data: () => ({ PROTOCOL_DEFAULT }),
-  computed: mapState({
-    aeternityApps: pathsToMetadata(aeternityAppsPaths),
-    bookmarkedApps: pathsToMetadata(['example.com']),
+  data: () => ({
+    PROTOCOL_DEFAULT,
+    wallet: null,
+    bookmarkedAppsPaths: [],
   }),
+  computed: mapState({
+    aeternityApps: (state, getters) => pathsToMetadata(aeternityAppsPaths, getters),
+    bookmarkedApps(state, getters) {
+      return pathsToMetadata(this.bookmarkedAppsPaths, getters);
+    },
+  }),
+  methods: {
+    getClickHandler(path) {
+      return this.wallet ? this.wallet.navigate(path) : undefined;
+    },
+    getLink(path) {
+      return this.wallet ? undefined : `${PROTOCOL_DEFAULT}//${path}`;
+    },
+  },
+  async mounted() {
+    this.wallet = await RpcClient.compose({
+      deepConfiguration: { Ae: { methods: ['navigate', 'bookmarkedApps', 'languageCode'] } },
+    })();
+    this.$store.commit('languages/setActiveCode', await this.wallet.languageCode());
+    this.bookmarkedAppsPaths = await this.wallet.bookmarkedApps();
+  },
 };
 </script>
 
