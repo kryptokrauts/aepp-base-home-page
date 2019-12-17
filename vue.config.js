2f1b9aa0a6071e836201cdc4eae7b494c7e1689d
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-12-17 13:41:22 +0100

    Fix running on aepps.tech

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.CI ? '/aepp-base-home-page/' : '/',
   chainWebpack: config => config
     .module.rule('svg')
     .uses.clear().end()
