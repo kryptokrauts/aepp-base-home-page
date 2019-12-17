48f67545262279c2b08a6b15e066e15f56d9ccb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-17 17:04:51 +0000

    Deploy aeternity/aepp-base-home-page to github.com/aeternity/aepp-base-home-page.git:gh-pages

---
+++ precache-manifest.3cbb724a961e8557f1ad83058c4c8a96.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a3983c9a553685a6753b7ccfd7b63d",
+    "url": "/404.html"
+  },
+  {
+    "revision": "490704c24585e5b1740deeac2a9a6960",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "e7ea9963fdb90834dadf322cbfc2743f",
+    "url": "/apps.json"
+  },
+  {
+    "revision": "81dd15237ca5abaff43c",
+    "url": "/css/app.99437688.css"
+  },
+  {
+    "revision": "6d21ddecff59ea808b72",
+    "url": "/css/chunk-vendors.ea73ed3a.css"
+  },
+  {
+    "revision": "85e9a84d651c191fc0dc03f204a6bda1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81dd15237ca5abaff43c",
+    "url": "/js/app.b9170d0d.js"
+  },
+  {
+    "revision": "6d21ddecff59ea808b72",
+    "url": "/js/chunk-vendors.93a81cbb.js"
+  },
+  {
+    "revision": "8680d5475203ddbe4b36",
+    "url": "/js/locale-cn.27c04285.js"
+  },
+  {
+    "revision": "eff7cc50724b171d6157",
+    "url": "/js/locale-es.6e1900a6.js"
+  },
+  {
+    "revision": "77da90fadc6309103dec",
+    "url": "/js/locale-ru.2ac33a5d.js"
+  },
+  {
+    "revision": "602bddd7d2606414f927e4fa14a153c0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
